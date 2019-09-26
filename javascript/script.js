e56e075e7c5177cbea2ce5cf38f4b348e553da5a
jameshuntcode <jameshuntwork1997@yahoo.com>
2019-09-26 22:28:36 +0100

    experience section layout complete - just need to fill in text content

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,23 @@
 $(document).ready(function () {
-    // Set the height of the slideshow / set position of text
+    // Set the height of the welcome image container
     var pageHeight = $(window).height() - 50;
-    $("#slideshow").css('height', pageHeight + 'px');
-    $('#slideshow h3').css('marginBottom', (pageHeight / 2.5) + 'px');
+    $("#welcome").css('height', pageHeight + 'px');
 
-    // Ensure, even when the page is resized, the size remains consistent.
+
+    // Ensure, even when the page size is changed, the container proportions remain consistent.
     $(window).resize( function () {
-        var pageHeight = $(window).height() - 50;
-        $("#slideshow").css('height', pageHeight + 'px');
-        $('#slideshow h3').css('marginBottom', (pageHeight / 2.5) + 'px');
+        let pageHeight = $(window).height() - 50;
+        $("#welcome").css('height', pageHeight + 'px');
     });
 
-    // Project-Highlights shake on hover:
-    $('.highlight-image, .skill-set-image').hover(function () {
-        $(this).addClass('animated pulse').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-            $(this).removeClass('animated pulse');
-        });
-    });
+
 
     // Scroll to page elements from the navigation bar:
     $('#navbar a').on('click', function (e) {
         e.preventDefault();
 
-        var linkClicked = $(this).text().toUpperCase();
-        var selectedElement;
+        let linkClicked = $(this).text().toUpperCase();
+        let selectedElement;
 
         switch (linkClicked) {
 
@@ -33,17 +27,11 @@ $(document).ready(function () {
             case "ABOUT":
             selectedElement = $("#about-me");
             break;
-            case "SKILLS":
-            selectedElement = $("#my-skills");
+            case "EXPERIENCE":
+            selectedElement = $("#experience");
             break;
-            case "HIGHLIGHTS":
-            selectedElement = $("#projects");
-            break;
-            case "INTERESTS":
-            selectedElement = $("#current-interests");
-            break;
-            case "HIRE ME":
-            selectedElement = $("#hire-me");
+            case "HOBBIES":
+            selectedElement = $("#hobbies");
             break;
             case "CONTACT":
             selectedElement = $("#contact-me");
@@ -51,59 +39,10 @@ $(document).ready(function () {
 
         }
 
-        var scrollTo = function(where) {
+        let scrollTo = function(where) {
             $('html, body').animate({scrollTop: where.offset().top - 50}, 1500);
-        }
+        };
 
         scrollTo(selectedElement);
     });
-
-    // Scroll to page elements from the footer:
-    $('#footer a').on('click', function (e) {
-        e.preventDefault();
-
-        var linkClicked = $(this).text().toUpperCase();
-        var selectedElement;
-
-        switch (linkClicked) {
-
-            case "TOP OF PAGE":
-            selectedElement = $("#navbar");
-            break;
-            case "ABOUT ME":
-            selectedElement = $("#about-me");
-            break;
-            case "MY SKILL SET":
-            selectedElement = $("#my-skills");
-            break;
-            case "PROJECT HIGHLIGHTS":
-            selectedElement = $("#projects");
-            break;
-            case "WORK & INTERESTS":
-            selectedElement = $("#current-interests");
-            break;
-            case "HIRE ME":
-            selectedElement = $("#hire-me");
-            break;
-            case "CONTACT ME":
-            selectedElement = $("#contact-me");
-            break;
-            case "EMAIL ME":
-            selectedElement = $("#contact-me");
-            break;
-
-        }
-
-        var scrollTo = function(where) {
-            $('html, body').animate({scrollTop: where.offset().top - 50}, 1500);
-            console.log(where.offset().top);
-        }
-
-        scrollTo(selectedElement);
-    });
-
-    // Set Copyright Date
-    var copyrightNotice = $('#copyright');
-    var year = new Date().getFullYear();
-    copyrightNotice.html("© " + year + " James Hunt");
 });
